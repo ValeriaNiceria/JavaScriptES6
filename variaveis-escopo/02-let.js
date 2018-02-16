a3cdb9361cdfc1d2b5eb19b741a3e5653d6b4ea8
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-16 12:36:25 -0200

    Variável let

---
+++ variaveis-escopo/02-let.js
@@ -0,0 +1,11 @@
+var animal = 'cat';
+console.log(animal);//cat
+
+// let animal = 'cat'; //VAI APRESENTAR UM ERRO - POIS A VARIÁVEL 'animal' JÁ FOI DECLARADA
+
+{
+    let animal = 'dog';
+    console.log(animal);//dog
+}
+
+console.log(animal);//cat
