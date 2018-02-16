bdab466e281911b694145ee504e0f9bafdb07c64
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-16 13:47:59 -0200

    Temporal Dead Zone

---
+++ variaveis-escopo/04-temporal-dead-zone.js
@@ -0,0 +1,9 @@
+console.log(cat); //undefined
+var cat = 'miau'; 
+
+
+console.log(dog); // Error -> dog is not defined
+let dog = 'au-au';
+
+console.log(car); // Error -> car is not defined
+const car = 'vrum';
