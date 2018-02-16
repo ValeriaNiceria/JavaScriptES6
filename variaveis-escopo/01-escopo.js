f577301443a042dc01c20fb0fb69deccff975643
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-16 12:22:13 -0200

    Escopo

---
+++ variaveis-escopo/01-escopo.js
@@ -0,0 +1,9 @@
+function sayAnimal() {
+    var animal;
+    animal = 'cat';
+    console.log(animal);
+}
+
+sayAnimal();
+
+console.log(animal); //VAI ACONTECER UM ERRO - POIS A VARIÁVEL NÃO EXISTE NO ESCOPO GLOBAL
